782cfc0d90c0ad3b8b701ee7bded02351f300f24
selfsame <joseph.parker.1@gmail.com>
2015-11-21 13:50:31 -0500

    basic arcadia repl using python UDP socket

--- arcadia_repl.py
+++ arcadia_repl.py
@@ -1,46 +1,80 @@
-import json
-import os.path
-import re
 import sublime
-import sublime_plugin
+import socket
 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 11211
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(False)
+G = {"prompt": 0, "hist": 0}
+history = []
 
-instance = []
+def create_repl(window):
+    current_view = window.active_view()
+    (group, index) = window.get_view_index(current_view)
+    repl = window.new_file()
+    repl.set_name("*REPL* (arcadia)")
+    repl.settings().set("arcadia_repl", True)
+    repl.set_scratch(True)
+    repl.set_syntax_file("Packages/Clojure/Clojure.tmLanguage")
+    window.set_view_index(repl, group + 1, len(window.views_in_group(group + 1)))
+    window.focus_view(current_view)
+    repl.run_command("arcadia_repl_clear")
+    return repl
 
-class ReplView():
-    def __init__(self, view):
-        self.view = view
+def get_repl(window):
+    for v in window.views():
+        if v.name() == "*REPL* (arcadia)": return v
 
-class ArcadiaRepl(sublime_plugin.EventListener):
+def entered_text(view): return view.substr(sublime.Region(G["prompt"], view.size()))
 
-    def on_load(self, view):
-        print("ArcadiaRepl on_load")
+def send_repl(text):
+    if text == "": 
+        text = " "
+    elif len(history) == 0 or (G["hist"] == 0 and history[-1] != text):
+        history.append(text)
+    G["hist"] = 0
+    sock.sendto(text.encode('utf-8'), (UDP_IP, UDP_PORT))
 
-    def _resolve_window(self, view):
-        window = view.window()
+def update(window):
+    repl = get_repl(window)
+    try:
+        data = sock.recvfrom(1024)[0].decode("utf-8").replace("=>", ">", 1).replace("\r", "")
+        repl.run_command("arcadia_repl_insert", {"data":"\n"+data})
+    except: None
+    sublime.set_timeout(lambda: update(window), 100)
 
-        if window:
-            return window
-
-        return sublime.active_window()
+def place_cursor_at_end(view):
+    view.sel().clear()
+    view.sel().add(sublime.Region(view.size(),view.size()))
+    G["prompt"] = view.size()
 
 class StartArcadiaReplCommand(sublime_plugin.TextCommand):
-
-    def __init__(self, view):
-        super().__init__(view)
-        self.window = view.window()
-
     def run(self, edit):
-        if len(instance) > 0:
-            print(instance[0].view)
-            return None
-        else:
-            (group, index) = self.window.get_view_index(self.view)
-            repl_window = self.window.new_file()
-            repl_window.set_name("*REPL* (arcadia)")
-            self.window.set_view_index(repl_window, group + 1, len(self.window.views_in_group(group + 1)))
-            self.window.focus_view(self.view)
-            instance.append(ReplView(repl_window))
-            print(instance[0].view)
-            view.window().run_command("repl_open",{"encoding":"utf8","type": "subprocess","cmd":["ping", "8.8.8.8"]})
-            return None
+        window = self.view.window()
+        repl = get_repl(window) or create_repl(window)
+        window.focus_view(get_repl(window))
+        update(window)
+
+class ArcadiaReplEnterCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        send_repl(entered_text(self.view))
+
+class ArcadiaReplInsertCommand(sublime_plugin.TextCommand):
+    def run(self, edit, data):
+        repl = get_repl(self.view.window())
+        repl.insert(edit,repl.size(), data)
+        place_cursor_at_end(repl)
+
+class ArcadiaReplClearCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        repl = get_repl(self.view.window())
+        repl.replace(edit,sublime.Region(0, repl.size()), ">")
+        place_cursor_at_end(repl)
+
+class ArcadiaReplHistoryCommand(sublime_plugin.TextCommand):
+    def run(self, edit, i=0):
+        repl = get_repl(self.view.window())
+        if (len(history) * -1) <= G["hist"] + i < 0:
+            G["hist"] += i
+            print(history[G["hist"]])
+            repl.replace(edit,sublime.Region(G["prompt"], repl.size()), history[G["hist"]])
