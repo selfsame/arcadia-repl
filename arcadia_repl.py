5d02d0390b0deb90e665614e235204ffb742f330
selfsame <joseph.parker.1@gmail.com>
2015-11-22 09:15:31 -0500

    rewriting Clojure tmLanguage via AAAPackage YAML converter

--- arcadia_repl.py
+++ arcadia_repl.py
@@ -1,5 +1,5 @@
-import sublime
-import socket
+import sublime, sublime_plugin
+import re, socket
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 11211
@@ -14,8 +14,9 @@ def create_repl(window):
     repl = window.new_file()
     repl.set_name("*REPL* (arcadia)")
     repl.settings().set("arcadia_repl", True)
+    repl.settings().set("scope_name", "source.repl")
     repl.set_scratch(True)
-    repl.set_syntax_file("Packages/Clojure/Clojure.tmLanguage")
+    repl.set_syntax_file("Packages/arcadia-repl/Clojure.tmLanguage")
     window.set_view_index(repl, group + 1, len(window.views_in_group(group + 1)))
     window.focus_view(current_view)
     repl.run_command("arcadia_repl_clear")
@@ -35,18 +36,29 @@ def send_repl(text):
     G["hist"] = 0
     sock.sendto(text.encode('utf-8'), (UDP_IP, UDP_PORT))
 
+def format_input_text(text):
+    res = text.replace("=>", ">", 1).replace("\r", "")
+    error = re.findall(r"(\w[^:\W\n]+): ([^\n]+)\n", res)
+    if len(error) > 0:
+        _ns = re.split("\n", res)[-1]
+        res = "".join(re.split("\n", res)[1:-1]).replace("  at ", "\n").replace(" in ", "\n  ").replace(" (", "\n  (")
+        res = ":" + str(error[0][0]) + "\n\""+error[0][1] + "\"" + res +"\n"+_ns
+    return res 
+
 def update(window):
     repl = get_repl(window)
     try:
-        data = sock.recvfrom(1024)[0].decode("utf-8").replace("=>", ">", 1).replace("\r", "")
+        data = format_input_text(sock.recvfrom(1024)[0].decode("utf-8"))
         repl.run_command("arcadia_repl_insert", {"data":"\n"+data})
     except: None
     sublime.set_timeout(lambda: update(window), 100)
 
 def place_cursor_at_end(view):
     view.sel().clear()
-    view.sel().add(sublime.Region(view.size(),view.size()))
+    end_region = sublime.Region(view.size(),view.size())
+    view.sel().add(end_region)
     G["prompt"] = view.size()
+    view.show(end_region)
 
 class StartArcadiaReplCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -79,20 +91,55 @@ class ArcadiaReplHistoryCommand(sublime_plugin.TextCommand):
             print(history[G["hist"]])
             repl.replace(edit,sublime.Region(G["prompt"], repl.size()), history[G["hist"]])
 
+def format_transfered_text(view, text):
+    namespacedecl = view.find(r"^[^;]*?\(", 0)
+    if namespacedecl and view.scope_name(namespacedecl.end()-1).startswith("source.clojure meta.function.namespace.clojure"):
+        namespacedecl = view.extract_scope(namespacedecl.end()-1)
+        pos = namespacedecl.begin() + 3
+        while pos < namespacedecl.end():
+            namespace = view.find(r"[\}\s][A-Za-z\_!\?\*\+\-][\w!\?\*\+\-:]*(\.[\w!\?\*\+\-:]+)*", pos)
+            if not namespace:
+                break
+            elif view.scope_name(namespace.begin() + 1).startswith("source.clojure meta.function.namespace.clojure entity.name.namespace.clojure"):
+                text = "(do (in-ns '" + view.substr(namespace)[1:] + ")" + text + ")"
+                break
+            else:
+                pos = namespace.end()
+    return text
+
 class ArcadiaReplTransferCommand(sublime_plugin.TextCommand):
     def run(self, edit, scope="block"):
         repl = get_repl(self.view.window())
-        text = ""
-        sel = []
+        regions, sel = [],[]
         for region in self.view.sel(): sel.append(region)
         if scope == "block": self.view.run_command("expand_selection", {"to": "brackets"})
         for idx in range(len(self.view.sel())):
             if scope == "selection":
-                text += self.view.substr(self.view.sel()[idx])
+                s = self.view.sel()[idx]
+                regions.append(sublime.Region(s.begin(), s.end()))
             elif scope == "block":
-                text += self.view.substr(sublime.Region(self.view.sel()[idx].a - 1, self.view.sel()[idx].b + 1))
+                regions.append(sublime.Region(self.view.sel()[idx].a - 1, self.view.sel()[idx].b + 1))
             elif scope == "file":
-                text = self.view.substr(sublime.Region(0, self.view.size()))
+                regions = [sublime.Region(0, self.view.size())]
+        for pair in regions: 
+            print(pair)
+            for text in find_blocks(self.view, pair.a, pair.b):
+                print(text)
+                send_repl(format_transfered_text(self.view, text))
         self.view.sel().clear()
         for region in sel: self.view.sel().add(region)
-        send_repl(text)
+    
+def find_blocks(view, start, end):
+    idx, depth = start, 0
+    regions, res = [], []
+    while idx < end:
+        enclosure = view.find(r"\"[^\"]*\"|;[^\n]*\n|\(|\[|\{|\)|\]|\}", idx)
+        if not enclosure: break
+        if view.substr(enclosure) in ("(", "[", "{"):
+            if depth == 0: res.append(enclosure.a)
+            depth += 1
+        elif view.substr(enclosure) in (")", "]", "}"):
+            depth -= 1
+            if depth == 0: regions.append(view.substr(sublime.Region(res.pop(), enclosure.b)))
+        idx = enclosure.end()
+    return regions
---
+++ test.cljs
@@ -0,0 +1,129 @@
+(ns learnnext.core
+	(:require
+		[om.next :as om :refer-macros [defui]]
+		[om.next.protocols :as p]
+		[om.dom :as dom]
+		[pdf.core :refer [and* or* not*] :refer-macros [defpdf pdf]]
+		[heh.core :as heh :refer-macros [html]]
+		[dollar.bill :as $ :refer [$]]
+		[learnnext.data :as data])
+	(:use [cljs.pprint :only [pprint]]))
+ 
+(enable-console-print!)
+ 
+($/append ($ "head") 
+	($ "<style>#chart{font-size:.7em; font-family:courier;} 
+			.column{display:inline-block; width:150px; border-right:1px solid black; padding:.2em .5em; border-bottom:1px dotted silver;}</style>"))
+
+(ns rainbow.sexpress)
+
+(quote [
+( ( ( ( ))))
+[[(((([[[]]]))))[[[] []]]]]
+((((()))))
+(((())))])
+
+'([(((([[][{[(())] ({([])[]})}] ]))))])
+'( ( ( ( ( ( ( ( ( ) ) ) ) ) ) ) ) 
+'([([([([([([([([([( )])])])])])])])])])
+'((((((()))))))
+'[() (())  [ (([([()])]))]]
+'(((( [[ [[ (( [ ( [ ()])]))]]]]))))[ [[[] ]]]
+
+;asdfjhasdf
+
+
+
+
+
+
+(def app-state (atom {
+	:app/title "Main Sequence Stars"
+	:stars (into {} 
+		(map 
+			(fn [[class rgb hex]] 
+				{class {:class class :color/rgb rgb :color/hex hex}})
+		data/stellar-colors))}))
+  
+(def stars? #{:stars})
+(def rgb? #(re-find #"rgb$" (str %)))
+ 
+ (comment 
+(defn read [data k]
+	(if-let [local (get  @(:state data) k)]
+    {:value local}
+    {:value :not-found})))
+
+
+(defpdf read)
+
+(pdf read [data k]
+  (get @(:state data) k :not-found))
+
+(pdf read [data k _]
+	(get @(:state data) k :not-found))
+
+(pdf read [data k props]
+  {props (and* :start :end)}
+  (let [local (get  @(:state data) k)
+        {:keys [start end]} props]
+    {:value (take (- end start) (drop start local))}))
+
+(pdf read [data k _]
+	{k #{:app/title}}
+	{:value (reverse (get @(:state data) k))})
+
+ 
+(defn mutate [_ _ _] {})
+
+
+(defui Star
+  static om/IQuery
+  (query [this] '[:class :color/rgb :color/hex])
+	Object
+	(render [this]
+		(let [props (om/props this)]
+			(html
+				(<div.star 
+					(<span.class.column (:class props))
+					(<span.rgb.column 
+						(style {:background (:color/hex props)})
+						(str "rgb" (into '() (:color/rgb props))))
+					(<span.class.column 
+						(style {:background (:color/hex props)})
+						(:color/hex props)) )))))
+
+(def star (om/factory Star))
+
+(defui Widget
+	static om/IQueryParams
+  (params [this]
+    {:start 0 :end 5})
+  static om/IQuery
+  (query [this] '[:app/title (:stars {:start ?start :end ?end})])
+	Object
+	(render [this]
+		(let [props (om/props this)
+				  stars (:stars props)]
+			(html
+				(<h1#title (:app/title props) ) (<hr)
+        (map 
+          (fn [[k v]]
+            (<label (clj->js k) 
+            (<input 
+              (value (str v))
+              (onChange (fn [e] 
+                (om/set-params! this 
+                  (conj (om/params this) 
+                    {k (int (.. e -target -value))}))
+                (prn (om/params this)))))))
+          (om/params this))
+				(<div#chart
+					(map #(<span.column %) (clj->js (keys (last (last stars)))))
+					(map #(star (assoc (last %) :om-index (gensym))) stars))))))
+ 
+
+(def reconciler (om/reconciler {:state app-state :parser (om/parser {:read read :mutate mutate})}))
+
+(om/add-root! reconciler Widget (first ($ "#app")))
+ 
